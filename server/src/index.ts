37c05680f77557dc8bf34711cc3fd3b763f032a3
Mridul Khandelwal <khandelwalmridul03@gmail.com>
2024-12-28 14:00:39 +0530

    Initialize Express server with MongoDB connection and middleware setup

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './config/db';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL ?? 'http://localhost:5173',
+		credentials: true,
+	})
+);
+
+app.get('/', (req, res) => {
+	res.send('Hello World');
+});
+
+// Connect to mongoDB
+connectDB();
+// Only starts the server once we're connected to mongoDB
+mongoose.connection.once('open', () => {
+	console.log(`Connected to mongoDB`);
+	app.listen(PORT, () => {
+		console.log(`Server is running on port :${PORT}`);
+	});
+});
